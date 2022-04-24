cb22685820101d95b2f0f781ae432019232bf8b1
AshwinTomar <ashwintomar04@gmail.com>
2022-04-25 00:17:43 +0530

    first commit

---
+++ client/src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2>DO YOU LIKE OFFERS ?</h2>
+              <p>SIGN UP NOW TO GET BEST OFFERS IN CAMPUS !!</p>
+              <form className="form-section">
+                <input placeholder="Your Email..." name="email" type="email" />
+                <input value="Yes. I want!" name="subscribe" type="submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;
---
+++ client/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>CALL US</h5>
+            <p>+91 1234567890</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>LOCATION</h5>
+            <p>CHANDIGARH UNIVERSITY</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>MAIL</h5>
+            <p>20BCS6180@cuchd.in</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
